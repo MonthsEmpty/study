ce33fc3d8f13b4119a8b79a148f25f10bace4673
MonthsEmpty <615109771@qq.com>
2016-09-02 16:49:20 +0800

    0902

--- src/test/com/Test.java
+++ src/test/com/Test.java
@@ -1,22 +1,32 @@
 package test.com;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
 public class Test {
 
 	public static void main(String[] args) {
-		/*Calendar cl = Calendar.getInstance();
-		cl.set(Calendar.HOUR_OF_DAY, 1);
-		cl.set(Calendar.MINUTE, 0);
-		cl.set(Calendar.SECOND, 0);
-		Date dt = cl.getTime();
-		System.out.println(dt);*/
-		
+		DateFormat startFormat = new SimpleDateFormat("yyyy-MM-01 00:00:00");
+		DateFormat endFormat = new SimpleDateFormat("yyyy-MM-dd 00:00:00");
+		String startTime = "";
+		String endTime = "";
+		Calendar calendar = Calendar.getInstance();
+		if(calendar.get(calendar.DAY_OF_MONTH) == 1){
+			endTime = endFormat.format(calendar.getTime());
+			calendar.add(Calendar.MONTH, -1);
+			startTime = startFormat.format(calendar.getTime());
+		}else{
+			startTime = startFormat.format(new Date());
+			endTime = endFormat.format(new Date());
+		}
+		System.out.println("startTime:" + startTime);
+		System.out.println("endTime:" + endTime);
 	}
 
 	public void print() {
-		System.out.println();
+
 	}
 
 }
